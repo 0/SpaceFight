c960b79dc89ef9ff3888b2fc48bfbaa62b53d47f
drobe <dee.ro.be@gmail.com>
2012-01-27 22:14:45 -0500

    Scanline process updated to do less things per cycle.

--- scanline.py
+++ scanline.py
@@ -1,19 +1,28 @@
+import pygame
+
 class Scanline():
-    def __init__(self,skip,speed,columns,rows):
+    def __init__(self,skip,speed,width,height,color):
         self.line = 0
         self.offset = 0
         self.skip = skip
         self.speed = speed
-        self.rows = rows
-        self.columns = columns
+        self.height = height
+        self.width = width
+        self.color = color
+        self.line_surface = pygame.Surface((width, height))
+        self.line_surface.convert_alpha()
+        self.line_surface.set_colorkey((0,0,0))
+        
     def update(self):
-        lines = []
+        self.line_surface.fill((0,0,0))
         for _ in xrange(self.speed):
-            lines.append(((0,self.line),(self.columns,self.line)))
+            pygame.draw.line(self.line_surface,self.color,(0,self.line),(self.width,self.line),1)
             self.line+= self.skip
-            if self.line > self.rows:
+            if self.line > self.height:
                 self.offset+=1
                 self.line = self.offset
             if self.offset == self.skip:
                 self.offset = 0
-        return lines
+                
+        #pygame.draw.line(self.line_surface,self.color,line[0],line[1],1)
+        return self.line_surface
