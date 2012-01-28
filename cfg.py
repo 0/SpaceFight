c764e9cb059d99a7a960ea2d21540cc2faad3fe7
drobe <dee.ro.be@gmail.com>
2012-01-27 22:16:11 -0500

    Added additional parameters to be loaded from cfg.

--- cfg.py
+++ cfg.py
@@ -1,10 +1,17 @@
 # These values depend on the border images.
-DEFAULT_WIDTH = 800
-DEFAULT_HEIGHT = 600
-DEFAULT_BORDER_THICKNESS = 100
-DEFAULT_BOUNDS = {'left': 79, 'top': 49, 'right': 78, 'bottom': 63}
+DEFAULT_WIDTH               = 800
+DEFAULT_HEIGHT              = 600
+DEFAULT_BORDER_THICKNESS    = 100
+DEFAULT_BOUNDS              = {'left': 80, 'top': 50, 'right': 80, 'bottom': 65}
+DEFAULT_SCANLINE_COLOR      = (24,30,24)
+DEFAULT_SCANLINE_SKIP       = 6
+DEFAULT_SCANLINE_SPEED      = 2
+DEFAULT_FRAMERATE           = 60
 
-width = DEFAULT_WIDTH
-height = DEFAULT_HEIGHT
-border_thickness = None # Calculated value.
-fullscreen = False
+width                       = DEFAULT_WIDTH
+height                      = DEFAULT_HEIGHT
+border_thickness            = None # Calculated value.
+fullscreen                  = False
+scanlineColor               = DEFAULT_SCANLINE_COLOR
+scanlineSkip                = DEFAULT_SCANLINE_SKIP
+scanlineSpeed               = DEFAULT_SCANLINE_SPEED
